9c9addae648d4a9e0fac542017a29ae46e2e86f1
jwheller0013 <jwheller0013@yahoo.com>
2025-04-16 13:32:27 -0400

    Building on previous pull method

---
+++ DailyPull.py
@@ -0,0 +1,34 @@
+import urllib.request
+import json
+
+url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/"
+tok = "pLPWsbxSowNQurrZleAiABCJGkuJepeg"
+limit = 1000
+
+def make_request (url, filename, headers):
+    try:
+        req = urllib.request.Request(url, headers=headers)
+        with urllib.request.urlopen(req) as res:
+            data = json.loads(res.read().decode('utf-8'))
+            with open(filename, 'w') as f:
+                json.dump(data, f, indent=4)
+            print("Success")
+            return data.get('metadata', {}).get('totalCount', 0)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
+
+
+if __name__ == "__main__":
+    headers = {
+        'token': tok
+    }
+    offset_increment = limit
+
+    for i in range(40):
+        offset = i * offset_increment
+        api_url = f"{url}?limit={limit}&offset={offset}"
+        filename = f"locations_{i}.json"
+        make_request(api_url, filename, headers)
+        print(f"Attempting to create file: {filename}")
